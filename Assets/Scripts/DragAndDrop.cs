c3f141c0967162997c0b698c94ac781f703788fb
Vincent Perigois <dolsok@hotmail.fr>
2016-11-04 12:54:54 +0100

    no message

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragAndDrop : MonoBehaviour {
+
+	public GameObject selectedCube ;
+	private Vector3 cubePosition2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount >= 1)
+		{
+			foreach (Touch touch in Input.touches)	
+			{
+				Vector2 cubePosition = touch.position;
+				Vector3 cubePosition2 = new Vector3 (cubePosition.x, cubePosition.y, 0);
+			}
+		}
+	}
+	void FixedUpdate () {
+		selectedCube.transform.position = cubePosition2;
+	}
+}
---
+++ Assets/Scripts/DragTest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragTest : MonoBehaviour {
+
+	public GameObject selectedCube = null;
+	private Vector3 cubePosition;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			
+			Vector2 mouseCache = Input.mousePosition;
+			Ray rayPosition = Camera.main.ScreenPointToRay (mouseCache);
+			RaycastHit hitInfo;
+			bool touched = Physics.Raycast (rayPosition, out hitInfo);
+			if (touched) {
+				selectedCube = hitInfo.collider.gameObject;
+			}
+		}
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			//check si sur referent.
+			//sinon pos = pos init
+			selectedCube = null;
+		}
+		if (Input.GetMouseButton (0)) 
+		{
+			Vector2 mouseCache = Input.mousePosition;
+			//Vector3 cubePosition2 = new Vector3 (mouseCache.x, mouseCache.y, 0);
+			float dist = Vector3.Distance (Camera.main.transform.position, selectedCube.transform.position);
+			Vector3 cubePosition2 = Camera.main.ScreenToWorldPoint (Input.mousePosition) + dist * Camera.main.ScreenPointToRay (mouseCache).direction;
+			//bouger objet ici
+		}
+	}
+	void FixedUpdate() 
+	{
+		selectedCube.transform.position = cubePosition;
+	}
+}
