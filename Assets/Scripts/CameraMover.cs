df48ed195bf03a7a661458669724adeaee0c0c4e
MrMesk <mesk.utp@gmail.com>
2016-11-04 12:35:15 +0100

    Camera Zoom and Move Forward

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour 
+{
+	public float moveDistanceOnTap;
+	[Range(0f,1f)]
+	public float moveSpeed;
+	Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () 
+	{
+		targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.touchCount == 1) 
+		{
+			Touch doubleTapChecker = Input.GetTouch(0);
+			if (doubleTapChecker.tapCount >= 2) 
+			{
+				GoForward ();
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (targetPos != transform.position) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, targetPos, 0.5f);
+		}
+	}
+
+	void GoForward()
+	{
+		targetPos += Vector3.forward * moveDistanceOnTap;
+	}
+}
---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour 
+{
+	public float maxFOVValue;
+	public float minFOVValue;
+	public float zoomSpeed;
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (cam == null) 
+		{
+			cam = Camera.main;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.touchCount == 2) 
+		{
+			FoVChange (PinchManager());
+		} 
+	}
+
+	float PinchManager()
+	{
+
+		Touch touchZero = Input.GetTouch (0);
+		Touch touchOne = Input.GetTouch (1);
+
+		Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+		Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+		float prevTouchDeltaMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+		float touchDeltaMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+		return prevTouchDeltaMagnitude - touchDeltaMagnitude;
+
+
+	}
+	void FoVChange(float modifier)
+	{
+		float zoomedFoV = cam.fieldOfView + modifier;
+
+		zoomedFoV = Mathf.Clamp (zoomedFoV, minFOVValue, maxFOVValue);
+		//Debug.Log ("Cam FoV" + zoomedFoV);
+
+		cam.fieldOfView = zoomedFoV;
+	}
+}
