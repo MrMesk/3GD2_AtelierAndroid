50ed354fdd6dc1860b6e46537587b373bbbe481d
Ulkan <camilleleonard.j@gmail.com>
2016-11-08 12:01:49 +0100

    Finishing ColorMatcher
    Because I'm awesome and i won't work Friday

--- Assets/Scripts/ColorMatcher.cs
+++ Assets/Scripts/ColorMatcher.cs
@@ -4,25 +4,40 @@ using System.Collections;
 public class ColorMatcher : MonoBehaviour {
 
 	public Color meshColor;
-	public GameObject[] objets;
+
+	public GameObject arch;
+	public Color archColor;
+
+	public GameObject door;
+
+	public GameObject decorFolder;
+	public GameObject[] elems;
 
 	WebCamTexture webcamTexture;
 
+
 	// Use this for initialization
 	void Start () {
+		AddToObjets ();
 		webcamTexture = new WebCamTexture();
 		webcamTexture.Play();
+		//archColor = Color.red;  //test color
+		archColor = new Color (Random.Range(0,1.1f), Random.Range(0,1.1f), Random.Range(0,1.1f));
+		arch.GetComponent<Renderer> ().material.color = archColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		meshColor = moyenne (webcamTexture.GetPixels());
 
-		for (int i = 0; i < objets.Length; i++) {
-			objets[i].GetComponent<Renderer> ().material.color = meshColor;
+		for (int i = 0; i < elems.Length; i++) {
+			elems[i].GetComponent<Renderer> ().material.color = meshColor;
 		}
+
+		TestColor ();
 	}
 
+	// Test the mid color of what the camera take
 	Color moyenne (Color[] pixels) {
 		float redSum = 0;
 		float greenSum = 0;
@@ -35,5 +50,26 @@ public class ColorMatcher : MonoBehaviour {
 		}
 
 		return new Color (redSum / pixels.Length, greenSum / pixels.Length, blueSum / pixels.Length);
+	}
+
+
+	// add all decor elements to elems 
+	void AddToObjets () {
+		elems = new GameObject[decorFolder.transform.childCount];
+
+		for (int i = 0; i < decorFolder.transform.childCount; i++) {
+			elems[i] = decorFolder.transform.GetChild (i).gameObject;
+		}
+	}
+
+
+	// test if the arch color is approximatly the same as the one of the elems
+	void TestColor () {
+		if ((Mathf.Abs (meshColor.r - archColor.r) + Mathf.Abs (meshColor.g - archColor.g) + Mathf.Abs (meshColor.b - archColor.b)) < 0.1 * 3) {
+			door.SetActive (false);
+		}
+		else {
+			door.SetActive (true);
+		}
 	}
 }
