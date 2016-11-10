c9981b301a7da808786d800b9ba424c2ab9461c1
VincentPerigois\Vincent Perigois <dolsok@hotmail.fr>
2016-11-10 12:20:58 +0100

    Drag'n'Drop fonctionnel. A faire : Convertir en commande Android

--- Assets/Scripts/DragTest.cs
+++ Assets/Scripts/DragTest.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class DragTest : MonoBehaviour {
 
 	public GameObject selectedCube = null;
+	public GameObject canalRéférent;
+	public float margeDerreur ;
 	private Vector3 cubePosition;
+
+	public Vector3 originalPosition;
 
 	// Use this for initialization
 	void Start () {
 
+		originalPosition = transform.position;
 	}
 
 	// Update is called once per frame
@@ -21,27 +26,36 @@ public class DragTest : MonoBehaviour {
 			Ray rayPosition = Camera.main.ScreenPointToRay (mouseCache);
 			RaycastHit hitInfo;
 			bool touched = Physics.Raycast (rayPosition, out hitInfo);
-			if (touched) {
+			if (touched) 
+			{
 				selectedCube = hitInfo.collider.gameObject;
 			}
 		}
-		if (Input.GetMouseButtonUp (0)) 
+		if (Input.GetMouseButtonUp (0) && selectedCube !=null) 
 		{
+			float checkDist = Vector3.Distance (selectedCube.transform.position, canalRéférent.transform.position);
+			if (checkDist < margeDerreur) {
+				selectedCube.transform.position = canalRéférent.transform.position;
+			}
+				else
+					selectedCube.transform.position = originalPosition;
+			
 			//check si sur referent.
 			//sinon pos = pos init
 			selectedCube = null;
 		}
-		if (Input.GetMouseButton (0)) 
-		{
+		if (Input.GetMouseButton (0) && selectedCube !=null) 
+			{
 			Vector2 mouseCache = Input.mousePosition;
 			//Vector3 cubePosition2 = new Vector3 (mouseCache.x, mouseCache.y, 0);
 			float dist = Vector3.Distance (Camera.main.transform.position, selectedCube.transform.position);
 			Vector3 cubePosition2 = Camera.main.ScreenToWorldPoint (Input.mousePosition) + dist * Camera.main.ScreenPointToRay (mouseCache).direction;
+			selectedCube.transform.position = new Vector3(cubePosition2.x, cubePosition2.y, selectedCube.transform.position.z);
 			//bouger objet ici
+			}
 		}
-	}
 	void FixedUpdate() 
 	{
-		selectedCube.transform.position = cubePosition;
+		//selectedCube.transform.position = cubePosition;
 	}
 }
