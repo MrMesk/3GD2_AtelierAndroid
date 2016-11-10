efbb4ca14bf1e8631c47449ec7a40fa99d9506fc
Ulkan <camilleleonard.j@gmail.com>
2016-11-10 12:05:57 +0100

    plop
    plop

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -5,13 +5,23 @@ public class CameraRotation : MonoBehaviour {
 
 	public float rotFactor;
 
+	public float initFactor;
+	Quaternion rotationInit;
+
 	// Use this for initialization
 	void Start () {
 		Input.gyro.enabled = true;
+
+		rotationInit = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (-Input.gyro.rotationRateUnbiased.x * rotFactor, -Input.gyro.rotationRateUnbiased.y * rotFactor, 0);
+
+		Debug.Log (Input.acceleration.x + " / " + Input.acceleration.y);
+		if (Input.acceleration.x > initFactor || Input.acceleration.y > initFactor) {
+			transform.rotation = rotationInit;
+		}
 	}
 }
