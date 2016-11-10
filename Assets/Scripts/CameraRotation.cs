b4b6ed94cf351730a6a92f32d9265413e7e25e81
Ulkan <camilleleonard.j@gmail.com>
2016-11-10 12:22:50 +0100

    bite
    bite

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -6,22 +6,21 @@ public class CameraRotation : MonoBehaviour {
 	public float rotFactor;
 
 	public float initFactor;
-	Quaternion rotationInit;
+	//Quaternion rotationInit;
 
 	// Use this for initialization
 	void Start () {
 		Input.gyro.enabled = true;
 
-		rotationInit = transform.rotation;
+		//rotationInit = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (-Input.gyro.rotationRateUnbiased.x * rotFactor, -Input.gyro.rotationRateUnbiased.y * rotFactor, 0);
 
-		Debug.Log (Input.acceleration.x + " / " + Input.acceleration.y);
-		if (Input.acceleration.x > initFactor || Input.acceleration.y > initFactor) {
+		/*if (Input.acceleration.x > initFactor) {
 			transform.rotation = rotationInit;
-		}
+		}*/
 	}
 }
