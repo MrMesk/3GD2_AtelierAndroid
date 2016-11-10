2ad90a92cc2416a2976afcc2be4715d7e33c78cb
MrMesk <mesk.utp@gmail.com>
2016-11-10 11:53:52 +0100

    Level Exit and locking device into portrait mode

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager GM;
+
+	public void NextLevel (string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	void Awake ()
+	{
+		if (GM != null)
+		{
+			Destroy(GM);
+		}
+		else
+			GM = this;
+
+		DontDestroyOnLoad(this);
+	}
+
+
+}
---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelExit : MonoBehaviour
+{
+	public Canvas endLevelText;
+	public string levelToLoad;
+	bool levelEnd;
+	// Use this for initialization
+	void Start ()
+	{
+		endLevelText.enabled = false;
+		levelEnd = false;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.touchCount == 1 && levelEnd)
+		{
+			GameManager.GM.NextLevel(levelToLoad);
+		}
+	}
+	void OnTriggerEnter()
+	{
+		levelEnd = true;
+		endLevelText.enabled = true;
+	}
+}
